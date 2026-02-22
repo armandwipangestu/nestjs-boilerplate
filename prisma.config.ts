5dfad47104f4f4344783041a48835f4a1912cbe1
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-02-22 07:15:56 +0700

    fix(ci): add database url dummy

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: 'bun prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL ?? 'postgresql://dummy:dummy@localhost:5432/dummy',
   },
 });
